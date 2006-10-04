51f26abc1f351dc28c49884706af752446c878a6
Robert Marianski <rob@marianski.com>
2006-10-04 19:49:35 +0000

    should have been a part of previous commit, r31392
    svn path=/remember/trunk/; revision=31394

---
+++ Products/remember/content/rememberuserchanger.py
@@ -0,0 +1,26 @@
+from zope.interface import implements
+
+from Products.membrane.interfaces import IMembraneUserChanger
+
+from Products.remember.interfaces import IRememberUserChanger
+
+class RememberUserChanger(object):
+    """
+    adapter from IRememberUserChanger -> IMembraneUserChanger
+    """
+
+    implements(IMembraneUserChanger)
+
+    def __init__(self, context):
+        self.context = context
+
+    def doChangeUser(self, login, password, **kwargs):
+        """change the password for a given user"""
+        # currently ignore the kwargs, but can be useful in the future
+        IRememberUserChanger(self.context)._setPassword(password)
+
+    def setPassword(self, password):
+        """
+        set the password on the remember member object
+        """
+        IRememberUserChanger(self.context)._setPassword(password)
