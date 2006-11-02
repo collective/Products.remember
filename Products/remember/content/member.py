79f1c27f3ed9e5f7360d027c8e8d6b4e965ca9a2
Rob Miller <ra@burningman.com>
2006-11-02 01:41:16 +0000

    * adding BBB __implements__ code so that AT's installTypes will work
    svn path=/remember/trunk/; revision=32666

--- Products/remember/content/member.py
+++ Products/remember/content/member.py
@@ -609,6 +609,10 @@ class Member(BrowserDefaultMixin, BaseMember, atapi.BaseContent):
     security = ClassSecurityInfo()
     base_archetype = atapi.BaseContent
 
+    # BBB: needed to get AT's installTypes to work
+    __implements__ = BrowserDefaultMixin.__implements__ + \
+                     atapi.BaseContent.__implements__
+
     def __call__(self, *args, **kwargs):
         """
         Prevents infinite recursion when using member object as a
@@ -626,6 +630,10 @@ class FolderishMember(BrowserDefaultMixin, BaseMember, atapi.BaseFolder):
     """
     security = ClassSecurityInfo()
     base_archetype = atapi.BaseFolder
+
+    # BBB: needed to get AT's installTypes to work
+    __implements__ = BrowserDefaultMixin.__implements__ + \
+                     atapi.BaseFolder.__implements__
 
     def __call__(self, *args, **kwargs):
         """
