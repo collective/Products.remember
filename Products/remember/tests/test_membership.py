94aeb5cf8a2bfe53a65ed4b323caa15192f43f99
Andrew Burkhalter <andrewburkhalter@gmail.com>
2007-03-16 18:22:38 +0000

    adding test to prove that memberdata tool returns all possible membrane types in the list of notAddableTypes when a non-manager is logged into the site, see revision 39252 for more details
    svn path=/remember/trunk/; revision=39270

--- Products/remember/tests/test_membership.py
+++ Products/remember/tests/test_membership.py
@@ -11,6 +11,7 @@ from Products.remember.config import DEFAULT_MEMBER_TYPE
 
 from base import RememberTestBase
 
+
 class TestMembershipTool(RememberTestBase):
     """This test is a vestige from when remember implemented its own MembershipTool.
     (We're confirming that its removal isn't breaking anything.)"""
@@ -43,6 +44,41 @@ class TestMembershipTool(RememberTestBase):
         self.failUnless('Member' in mdtool.getNotAddableTypes())
         self.loginAsPortalOwner()
         self.failIf('Member' in mdtool.getNotAddableTypes())
+
+    def test_addCustomMemberTypeToFolderPermission(self):
+        """
+        verify that any custom ReMember-based objects
+        are addable only for managers as well.  Tests 
+        integration of the ReMember UI with the membrane 
+        tool's listMembraneTypes - Note: this could have
+        been done with the Generic Setup test profile 
+        or with Python.  The latter is done below.
+        """
+        self.types = self.portal.portal_types
+        
+        # Copy Member and make it AnotherMember in types tool
+        self.types.manage_pasteObjects(
+            self.types.manage_copyObjects('Member'))
+
+        self.types.manage_renameObject('copy_of_Member',
+            'AnotherMember')
+                
+        # Add the "AnotherMember" type to PortalMemberdata's allowed types
+        memberdatacontainer = self.types['MemberDataContainer']
+        if 'AnotherMember' not in memberdatacontainer.allowed_content_types:
+            list(memberdatacontainer.allowed_content_types).append('AnotherMember')
+        
+        # Make sure it's listed as a Membrane Type
+        self.portal.membrane_tool.registerMembraneType('AnotherMember')
+        
+        # Prove that listMembraneTypes is providing correct values to
+        # the memberdata tools implementation of getAllowedMemberTypes
+        self.login('portal_member')
+        mdtool = getToolByName(self.portal, 'portal_memberdata')
+        self.failUnless('AnotherMember' in mdtool.getNotAddableTypes())
+        self.loginAsPortalOwner()
+        self.failIf('AnotherMember' in mdtool.getNotAddableTypes())
+
 
 def test_suite():
     suite = unittest.TestSuite()
