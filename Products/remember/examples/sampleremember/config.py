bf66486d383d183e796fc3f988a402e161033d54
Ross Patterson <me@rpatterson.net>
2009-03-20 20:24:18 +0000

    * Move the sampleremember package to it's own package to stop interfering with the test runner
    svn path=/Products.remember/trunk/; revision=82822

--- Products/remember/examples/sampleremember/config.py
+++
@@ -1,4 +0,0 @@
-PROJECT_NAME = 'sampleremember'
-SKINS_DIR = 'skins'
-GLOBALS = globals()
-DEFAULT_MEMBER_TYPE = 'SampleRemember'
--- Products/remember/examples/sampleremember/setuphandlers.py
+++
@@ -1,8 +0,0 @@
-from Products.remember.utils import getAdderUtility
-from config import DEFAULT_MEMBER_TYPE
-
-def setupNewDefaultMember(context):
-    """ Setup preferred default_member_type """
-    portal = context.getSite()
-    addr = getAdderUtility(portal)
-    addr.default_member_type = DEFAULT_MEMBER_TYPE
