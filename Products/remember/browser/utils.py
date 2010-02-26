793e9aa7300cc46ac3cee664eef7059443d1a327
Ross Patterson <me@rpatterson.net>
2010-02-26 11:06:37 +0000

    Fix a bug with the users control panel link
    svn path=/Products.remember/trunk/; revision=111796

--- Products/remember/browser/utils.py
+++ Products/remember/browser/utils.py
@@ -1,13 +1,14 @@
-from Products.CMFCore.utils import getToolByName
+from Acquisition import aq_parent
+from ZTUtils import make_query
 
-from Products.Five import BrowserView
+from Products.CMFCore.utils import getToolByName
 
 from Products.remember.interfaces import IRememberAuthProvider
 
-class PrefsUrlComputer(BrowserView):
+class PortalPrefsUrl(object):
     """
     Trivial view class that computes the appropriate URL for the
-    'preferences' link.
+    'preferences' link for the current user.
     """
     def __call__(self):
         mtool = getToolByName(self.context, 'portal_membership')
@@ -17,3 +18,22 @@ class PrefsUrlComputer(BrowserView):
             return "%s/edit" % member.absolute_url()
         portal_url = getToolByName(self.context, 'portal_url')()
         return "%s/personalize_form" % portal_url
+
+class RememberPrefsUrl(object):
+    """
+    Trivial view class that computes the appropriate URL for the
+    'preferences' link for a remember user.
+    """
+    def __call__(self):
+        return self.context.absolute_url()
+
+class MemberPrefsUrl(object):
+    """
+    Trivial view class that computes the appropriate URL for the
+    'preferences' link for a non-remember user.
+    """
+    def __call__(self):
+        member = aq_parent(aq_parent(self.context))
+        portal_url = getToolByName(self.context, 'portal_url')()
+        return "%s/prefs_user_details?%s" % (
+            portal_url, make_query(userid=member.getUserId()))
