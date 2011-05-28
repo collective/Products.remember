05a38f80b5936cc70dfa5fd6dbce9ac56704d753
Ken Manheimer <ken.manheimer@gmail.com>
2011-05-28 23:24:43 +0000

    explicitly import the unistall actions.xml step, since actions.xml seems to not be automatically supported.
    i suspect i'm missing something to get it to load automatically, but this
    works, and for what unistall/actions.xml currently does, it wouldn't cause
    problems if it were run twice.
    
    svn path=/Products.remember/branches/klm_track-membrane2/; revision=240126

--- Products/remember/Extensions/Install.py
+++ Products/remember/Extensions/Install.py
@@ -1,5 +1,4 @@
 from Products.CMFCore.utils import getToolByName
-from Products.remember.setuphandlers import uninstall
 
 def uninstall(self, portal, reinstall=False):
     setup_tool = getToolByName(portal, 'portal_setup')
@@ -8,9 +7,9 @@ def uninstall(self, portal, reinstall=False):
     # Revert some setup tool settings to vanilla plone values, so the tools
     # work.  This is a flimsy, because the vanilla plone settings could change,
     # but we have no real choice.  If other extensions change these settings,
-    # themselves, they'll have to be uninstalled and reinstalled to reestablish
-    # their values.  Adjustment method adapted from:
-#http://plone.org/documentation/kb/manually-removing-local-persistent-utilities
+    # themselves, they'll have to be reinstalled to reestablish their values.
+    # Adjustment method adapted from:
+    # http://plone.org/documentation/kb/manually-removing-local-persistent-utilities
 
     toolset = portal.portal_setup.getToolsetRegistry()
     if toolset._required.has_key('portal_registration'):
@@ -22,4 +21,8 @@ def uninstall(self, portal, reinstall=False):
 
     portal.portal_controlpanel.unregisterApplication('remember')
 
-    setup_tool.runAllImportStepsFromProfile('profile-%s' % profile)
+    uninstall_profile = 'profile-%s' % profile
+    setup_tool.runAllImportStepsFromProfile(uninstall_profile)
+    # XXX actions.xml seems not yet implemented for uninstall profiles, and
+    # when it eventually is, having what we're doing now run double won't hurt.
+    setup_tool.runImportStepFromProfile(uninstall_profile, 'actions', True)
