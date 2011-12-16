324c252afd831e7264db91d587b59c816e9dd7a7
Maurits van Rees <maurits@vanrees.org>
2011-12-16 22:14:05 +0000

    Back to development: 1.9.2
    svn path=/Products.remember/trunk/; revision=247379

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.9.1'
+version = '1.9.2dev'
 tests_require = ['collective.testcaselayer',
                  'plone.browserlayer',
                  'zope.testbrowser>3.3']
